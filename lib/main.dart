085b3afea6cd5e0aa914b4e271db1dc59fc8752d
김규현 <kgh2703@irlink.co.kr>
2022-02-11 09:35:53 +0900

    List 출력

--- lib/main.dart
+++ lib/main.dart
@@ -14,37 +14,45 @@ class MyApp extends StatelessWidget {
 
   Widget home() {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: Text("SSD"),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {
-                print("누름");
-              },
-              icon: Icon(Icons.mode_comment)),
-          IconButton(onPressed: () => print("asdf"), icon: Icon(Icons.money_off))
-        ],
-      ),
-      body: body()
-    );
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text("SSD"),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  print("누름");
+                },
+                icon: Icon(Icons.mode_comment)),
+            IconButton(
+                onPressed: () => print("asdf"), icon: Icon(Icons.money_off))
+          ],
+        ),
+        body: body());
   }
 
   Widget body() {
     return Container(
       padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      child: cols(),
+      child: testList(),
     );
+  }
+
+  Widget testList() {
+    List<String> items = List.generate(101, (i) => "List 출력 테스트 $i");
+
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+            title: Text(items[i]),
+            subtitle: Text('$i번째 List'),
+          );
+        });
   }
 
   Widget cols() {
     return Column(
-      children: <Widget>[
-        rows(), rows(), rows()
-      ],
+      children: <Widget>[rows(), rows(), rows()],
     );
   }
 
